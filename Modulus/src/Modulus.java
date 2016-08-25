850218867e564597ccdb55ca41231ad8e9104821
Nicholas.Locascio <Nicholas.Locascio@MU-C1-08.mullen.pvt.k12.co.us>
2016-08-25 09:08:05 -0600

    Two Challenges Done

--- Modulus/src/Modulus.java
+++ Modulus/src/Modulus.java
@@ -1,10 +1,40 @@
+import java.util.Scanner;
 
 public class Modulus
 	{
-		public static void main(String[] args)
+	public static void main(String[] args)
+		{
+//		exercise1();
+		exercise2();
+//		exercise3();
+//		exercise4();
+		}
+//	public static void exercise1()
+//		{
+//		Scanner number = new Scanner(System.in);
+//		System.out.println("Enter any number.");
+//		int evenOdd = number.nextInt();
+//		if(evenOdd%2 == 1)
+//			{
+//			System.out.println("That's an odd number.");	
+//			}
+//		else
+//			{
+//			System.out.println("That's an even number.");
+//			}
+//		}
+	public static void exercise2()
+		{
+		Scanner year = new Scanner(System.in);
+		System.out.println("Enter a year.");
+		int leapYear = year.nextInt();
+		if(leapYear%4 == 0)
 			{
-				
-
+			System.out.println("That is a leap year.");
 			}
-
+		else
+			{
+			System.out.println("That is not a leap year.");
+			}
+		}
 	}
